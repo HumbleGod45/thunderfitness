069e5b4c8a88f7718a1eaa7d7c85ad23b76205c3
HumbleGod45 <athayafajar254@gmail.com>
2026-01-06 20:15:21 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,14 +3,23 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            [
+                'email' => 'admin@thunderfitness.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+           
+        ]);
     }
 }
